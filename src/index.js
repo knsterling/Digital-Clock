e0c916541f651dba509e853ab9843df0f2bc4cd0
knsterling <74157330+knsterling@users.noreply.github.com>
2021-01-28 18:00:16 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+function showTime() {
+  let date = new Date();
+  let h = date.getHours(); //0-23
+  let m = date.getMinutes(); //0-59
+  let s = date.getSeconds(); //0-59
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = h + ":" + m + ":" + s + " " + session;
+
+  // DOCUMENT IS AN OBJECT. getElementId IS A METHOD/FUNCTION. innerText IS A PROPERTY WHERE THE VALUE IS THE VARIABLE TIME
+  document.getElementById("MyClockDisplay").innerText = time;
+
+  document.getElementById("MyClockDisplay").textContent;
+
+  // setTimeout(funtion[, delay])
+  setTimeout(showTime, 1000);
+}
+
+showTime();
